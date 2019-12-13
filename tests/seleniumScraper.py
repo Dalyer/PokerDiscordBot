5ff736f093c6130cb7ad7b1354d9ba6abe3251a1
Dalyer <dseiler@sfu.ca>
2019-12-13 13:20:54 -0800

    updated conditions

--- tests/seleniumScraper.py
+++ tests/seleniumScraper.py
@@ -13,16 +13,37 @@ log_button.click()
 
 
 game_log = driver.find_elements_by_css_selector("div:nth-child(1) div:nth-child(1) div.main-container.two-color div.log-ctn > div.log-viewer")
-game_data = {}
+
 
 log = game_log[0].text.split('\n')
 
-log.reverse()
+log.reverse()       # reverse order so it is sequential
+game_data = {}
+seq_num = -1
 for line in log:
+    seq_num += seq_num
     action = line.split()
+    # find integer values
+    value = 0
+    action_type = ""
+    for i in action:
+        if i.isdigit():
+            value = i
+    # get action type
+    for i in action:
+        if i == 'calls':
+            action_type = 'calls'
+        elif i == 'raises':
+            action_type = 'raises'
+        elif i == 'posts':
+            action_type = 'blind'
+        elif i == 'wins' or i == 'gained':
+            action_type = "win"
     # check for stack increases
-    if 'wins' in action or 'gained' in action:
-        print("wins or gained found")
+    if 'wins' in action:
+        newdict = dict(time=action[0], player=action[1], stack_change=value, action_type=action_type, seq_num=seq_num)
+    elif 'gained' in action:
+        pass
     # check for big and little blinds
     elif 'posts' in action:
         print("big/little blinds found")
